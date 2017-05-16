1552990b8e3f33ce8867233b23b6d25143001b5a
WOWOANDYOU <WOWOANDYOU@gmail>
2017-05-16 10:24:00 +0800

    junit 增加对ClothDaoImple的测试

---
+++ src/junit/test/ClothDaoTest.java
@@ -0,0 +1,26 @@
+package junit.test;
+
+import org.junit.Test;
+
+import cn.edu.zhku.jsj.dao.impl.ClothDaoImpl;
+import cn.edu.zhku.jsj.daomain.Cloth;
+
+public class ClothDaoTest {
+	@Test
+	public void addTest(){
+		Cloth cloth = new Cloth();
+		cloth.setClothname("秋衣男款");
+		cloth.setColor("黑色");
+		cloth.setDescription("物美价廉");
+		cloth.setImages("d://1.jpg");
+		cloth.setPrice(50);
+		cloth.setSize("S;M;L;XL;XXL");
+		cloth.setStore_id(1);
+		cloth.setTotalnum(59);
+		cloth.setType("衣服");
+		cloth.setVersion("潮流款;小清新;均码");
+		
+		ClothDaoImpl clothdao = new ClothDaoImpl();
+		clothdao.add(cloth);
+	}
+}

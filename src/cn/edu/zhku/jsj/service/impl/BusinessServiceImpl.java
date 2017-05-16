beccbbfb75680ab1df43ff3c753cb089e2455797
WOWOANDYOU <WOWOANDYOU@gmail>
2017-05-16 09:44:13 +0800

    抽取service接口

--- src/cn/edu/zhku/jsj/service/impl/BusinessServiceImpl.java
+++ src/cn/edu/zhku/jsj/service/impl/BusinessServiceImpl.java
@@ -14,8 +14,9 @@ import cn.edu.zhku.jsj.daomain.Food;
 import cn.edu.zhku.jsj.daomain.Store;
 import cn.edu.zhku.jsj.daomain.User;
 import cn.edu.zhku.jsj.factory.DaoFactory;
+import cn.edu.zhku.jsj.service.BusinessService;
 
-public class BusinessServiceImpl {
+public class BusinessServiceImpl implements BusinessService {
 	private UserDao user_dao = DaoFactory.getInstance().createDao(UserDao.class); 
 	private BookDao book_dao = DaoFactory.getInstance().createDao(BookDao.class); 
 	private FoodDao food_dao = DaoFactory.getInstance().createDao(FoodDao.class); 
@@ -27,34 +28,40 @@ public class BusinessServiceImpl {
 	private StoreDao store_dao = DaoFactory.getInstance().createDao(StoreDao.class); 
 	
 	//普通用户注册
+	@Override
 	public int adduser(User user){
 		int num = user_dao.add(user);
 		return num;
 	}
 	//普通用户变为 店家
+	@Override
 	public int addshopkeeper(User user){
 		int num = user_dao.add(user);
 		return num;
 	}
 	//开店铺   
+	@Override
 	public int addStore(Store store){
 		int num = store_dao.add(store);
 		return num;
 	}
 	
 	//添加商品 book
+	@Override
 	public int addBook(Book book){
 		int num = book_dao.add(book);
 		return num;
 	}
 	
 	//添加食物 
+	@Override
 	public int addFood(Food food){
 		int num = food_dao.add(food);
 		return num;
 	}
 	
 	//添加 衣服
+	@Override
 	public int addCloth(Cloth cloth){
 		int num = cloth_dao.add(cloth);
 		return num;

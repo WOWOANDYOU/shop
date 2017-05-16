0d0fca5d68cb7a1a6277f2f73f480b9d655686cd
WOWOANDYOU <WOWOANDYOU@gmail>
2017-05-16 18:38:30 +0800

    增加一个工具类，用来将结果集rs中的数据 封装到bean里边

---
+++ src/cn/edu/zhku/jsj/web/utils/ResultToBean.java
@@ -0,0 +1,84 @@
+package cn.edu.zhku.jsj.web.utils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.edu.zhku.jsj.daomain.Cloth;
+
+import com.mysql.jdbc.ResultSetMetaData;
+
+public class ResultToBean {
+	public static <T> List<T> getBeanList(Class<T> clazz, ResultSet rs)  {
+
+		Field field = null;
+		List<T> lists = new ArrayList<T>();
+		// 取得类里边的所有方法
+		try {
+			// 取得ResultSet列名
+			ResultSetMetaData rsmd = (ResultSetMetaData) rs.getMetaData();
+			// 获取记录集中的列数
+			int counts = rsmd.getColumnCount();
+			// 定义counts个String 变量
+			String[] columnNames = new String[counts];
+			// 给每个变量赋值 记录每个 字段的名字
+			for (int i = 0; i < counts; i++) {
+				//rsmd.getColumnLabel(i + 1); 给数据库 列名取个 别名
+				/*getColumnName返回的是sql语句中field的原始名字。getColumnLabel是field的SQL AS的值。
+				比如：select a.name as name,a.description as description,b.description as relatedDescription from a,b where ...
+				此时,getColumnName(3) == "description";而getColumnLabel(3) == "relatedDescription"。
+				如果你想将ResultSet的结果映射到HashMap中，注意一定使用getColumnLabel，而不要用getColumnName。*/
+				columnNames[i] = rsmd.getColumnLabel(i + 1);  // rsmd.getColumnLabel(i) 就是输出 响应的的列名
+				System.out.println(columnNames[i]);
+				//所以columnNaes 存放的就是 所有的列的名字
+			}
+
+			// 变量ResultSet
+			while (rs.next()) {
+				T t = clazz.newInstance();
+				// 反射, 从ResultSet绑定到JavaBean
+				for (int i = 0; i < counts; i++) {
+					// 根据 rs 列名 ，组装javaBean里边的其中一个set方法，object 就是数据库第一行第一列的数据了
+					Object value = rs.getObject(columnNames[i]);
+					System.out.println(value);
+					//这里是获取数据库字段的类型
+					Class<?> dbType = value.getClass();
+					System.out.println(dbType.getSimpleName());
+					//设置参数类型，此类型应该跟javaBean 里边的类型一样，而不是取数据库里边的类型
+					field = clazz.getDeclaredField(columnNames[i]);
+					Class<?> beanType = field.getType();
+					System.out.println(beanType.getSimpleName());
+					
+					//如果发生从数据库获取到得类型跟javaBean类型不同，先按String类型取  然后在转换为 具体的类型
+					if(beanType!=dbType){
+						value = rs.getString(columnNames[i]);
+						if(dbType.getSimpleName().equals("Integer")){
+							value = Integer.parseInt((String)value);
+						}else{
+							value =Float.parseFloat((String) value);
+						}
+				 	}
+					String setMethodName = "set" + firstUpperCase(columnNames[i]);
+					// 第一个参数是传进去的方法名称，第二个参数是 传进去的类型；
+					Method m = t.getClass().getMethod(setMethodName,beanType);
+					// 第二个参数是传给set方法数据；如果是get方法可以不写
+					m.invoke(t, value);
+				}
+				lists.add(t);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if(lists.get(0)==null){
+			System.out.println("!null");
+		}
+		return lists;
+	}
+	
+	//首写字母变大写
+	  public static String firstUpperCase(String old){
+ 		  return old.substring(0, 1).toUpperCase()+old.substring(1);
+	  }  
+}

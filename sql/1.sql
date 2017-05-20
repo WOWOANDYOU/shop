dc5abbd6e47047cc0ea2fdcb2c2f785890ce6640
WOWOANDYOU <WOWOANDYOU@gmail>
2017-05-20 20:52:04 +0800

    food店铺 上架商品成功

--- sql/1.sql
+++ sql/1.sql
@@ -4,3 +4,4 @@ select * from user;
 select * from store;
 select * from cloth;
 select * from food;
+delete from food where food_id = 2;
--- src/cn/edu/zhku/jsj/web/shopkeeper/AddFoodServlet.java
+++ src/cn/edu/zhku/jsj/web/shopkeeper/AddFoodServlet.java
@@ -13,6 +13,8 @@ import org.apache.commons.fileupload.FileUploadBase.FileSizeLimitExceededExcepti
 
 import cn.edu.zhku.jsj.daomain.Food;
 import cn.edu.zhku.jsj.formbean.FoodFormBean;
+import cn.edu.zhku.jsj.service.BusinessService;
+import cn.edu.zhku.jsj.service.impl.BusinessServiceImpl;
 import cn.edu.zhku.jsj.web.utils.WebUtil;
 
 @WebServlet("/servlet/AddFoodServlet")
@@ -23,7 +25,6 @@ public class AddFoodServlet extends HttpServlet {
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			String imgsavepath = this.getServletContext().getRealPath("/images");
-			System.out.println(imgsavepath);
 			try {
 				Map map2 = WebUtil.doUploadFood(request, imgsavepath);
 				FoodFormBean formbean = (FoodFormBean) map2.get("formbean");
@@ -33,9 +34,28 @@ public class AddFoodServlet extends HttpServlet {
 					return;
 				}else{
 					Food food = (Food) map2.get("food");
+					//图片 由于用 UUID 名  数据库 存放路径 大小设为 255 最多 可能只能存 5张图片
+					
+					
+food.setStore_id(1);//测试用 由于 还没有店主登录进来  所以先 手动 赋值为 一个存在的店铺  到时候要删除！！
+
+
+					BusinessService bus = new BusinessServiceImpl();
+					int num = bus.addFood(food);
+					if(num!=0){
+						request.setAttribute("message", "商品上架成功,3秒后返回 <meta http-equiv='refresh' content='3;url=/shop/pages/store.jsp'");
+						request.setAttribute("prepath", "/pages/store.jsp");
+						request.getRequestDispatcher("/pages/message.jsp").forward(request, response);
+						return;
+					}
+
 				}
 			} catch (FileSizeLimitExceededException e) {
 				e.printStackTrace();
+				request.setAttribute("message", "文件太大！");
+				request.setAttribute("prepath", "/pages/addFood.jsp");
+				request.getRequestDispatcher("/pages/message.jsp").forward(request, response);
+				return;
 			}
 	}
 

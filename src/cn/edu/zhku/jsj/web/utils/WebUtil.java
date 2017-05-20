0f80985d884f5baf925186b998d3cfd55bca7829
WOWOANDYOU <WOWOANDYOU@gmail>
2017-05-20 17:04:28 +0800

    在工具类中添加了用于将 form表单数据封装到 formbean中去的功能

--- src/cn/edu/zhku/jsj/web/utils/WebUtil.java
+++ src/cn/edu/zhku/jsj/web/utils/WebUtil.java
@@ -3,6 +3,7 @@ package cn.edu.zhku.jsj.web.utils;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,7 +31,7 @@ public class WebUtil {
 			/*	factory.setRepository(new File(request.getSession().getServletContext().getRealPath("/WEB-INF/temp")));*/
 				ServletFileUpload upload = new ServletFileUpload(factory);
 				upload.setHeaderEncoding("UTF-8");
-				upload.setFileSizeMax(1000*1000*500);
+				upload.setFileSizeMax(1000*1000*50);
 				List<FileItem> list = upload.parseRequest(request);
 				
 				String savefilename = "";// 用来保存 上传文件的文件名的 
@@ -181,5 +182,21 @@ public class WebUtil {
 		
 		private static String generateredUuidname(String filename) {
 			return UUID.randomUUID().toString()+"_"+filename;
+		}
+		
+		//从request 中获取数据 将form表单提交过来的数据 
+		public static <T> T getDatatoForm(HttpServletRequest request,Class<T> beanClass){
+			try{
+				T bean = beanClass.newInstance();
+				 Enumeration ele = request.getParameterNames();
+				 while(ele.hasMoreElements()){
+					String name = (String) ele.nextElement();
+					String value = request.getParameter(name);
+				 	BeanUtils.setProperty(bean, name, value);//调用 beanutils 开发包中的 设置 属性值的方法  对bean 对象 的 name变量记住的属性 赋予 value变量记住的值
+				 }
+				 return bean;
+			}catch(Exception e){
+				throw new RuntimeException(e);
+			}
 		}
 }
